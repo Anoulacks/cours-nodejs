5fec7eb78ef4d4fd2fed0c7b2bec8c63baea4395
thomas southasa <thomas.southasa@gmail.com>
2022-04-01 17:15:33 +0200

    install jsonwebtoken

---
+++ server/auth/index.mjs
@@ -0,0 +1,11 @@
+/*
+const auth = (name, password) => {
+    const file = fs.readFileSync('./controllers/auth/users.json', 'utf8')
+    const users = JSON.parse(file)
+    const user = users.fins((e) => e.name === name && e.password === password)
+
+    return generateAccessToken(user)
+}
+
+export { auth }
+*/
